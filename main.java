dfdb17c4f715d95adf8399f74ae5a360d542c3fd
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-06-26 15:58:23 +0200

    exercice in java

--- main.java
+++ main.java
@@ -1,32 +1,33 @@
 package learning_java;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		
-		//exo1
+		//exo1 count 1 to 10
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(i);
 		};
 		
-		//exo2
+		//exo2 sum 1 to 100
 		int y = 1;
 		for (int x = 1; x <= 100; x++) {
 			y = y + x;
 			System.out.println(y);
 		}
 		
-		//exo3 
+		//exo3 mutiplicate table
 		int nbx = 7;
 		for (int nby = 0; nby <= 10; nby++) {
 			int total = nbx * nby;
 			System.out.println(nbx + " x " + nby + " = " + total);
 		}
 		
-		//exo4
+		//exo4 bisextile years
 		int annee = 300;
 		if ((annee % 4 == 0) || (annee % 400 == 0 && annee % 100 != 0)) {
 			System.out.println("Bisextile");
@@ -34,12 +35,22 @@ public class main {
 			System.out.println("Non bisextile");
 		}
 		
-		//exo5
-		String mot = "kayak";
-		System.out.println(mot.length());
-		String[] words = mot.split("");
-		System.out.println(Arrays.toString(words));
-		System.out.println(words[0]);
-		
+		//exo5 palindrome words
+		String mot = "kok";
+		mot = mot.toLowerCase();
+		int j = mot.length()-1;
+		Boolean isTrue = true;
+		for (int i = 0; i < mot.length(); i++) {
+			if(mot.charAt(i) == mot.charAt(j)) {
+				j--;
+				System.out.println(j);
+			} else isTrue = false;
+		}
+		if(isTrue) {
+			System.out.println("oui palindrome");
+		} else {
+			System.out.println("non palindrome");
+		}
+	
 	}
 }
